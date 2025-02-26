5e0adb49745dbe331fc772dd5cb818fa0bd16e76
icsp <alfredocarlinogithub@gmail.com>
2025-02-26 23:01:34 +0100

    Fatto si che venga mostrata una preview se la tabella è vuota.

--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild, Renderer2, AfterViewInit} from '@angular/core';
+import {Component, ElementRef, ViewChild, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -7,27 +7,29 @@ import {Component, ElementRef, ViewChild, Renderer2, AfterViewInit} from '@angul
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
-export class TableComponent implements AfterViewInit {
+export class TableComponent {
 
   @ViewChild('columnsContainer') columnsContainer!: ElementRef;
   @ViewChild('rowsContainer') rowsContainer!: ElementRef;
 
-
   tableName = "New Table";
-  columnsNumber = 0;
-  rowsNumber = 0;
+  isEmpty = true;
 
 
   constructor(private renderer2: Renderer2) {}
 
 
-  ngAfterViewInit() {
-    this.columnsNumber = this.columnsContainer.nativeElement.children.length;
-    this.rowsNumber = this.rowsContainer.nativeElement.children.length;
+  getColumnsNumber(): number {
+    return this.columnsContainer.nativeElement.children.length - 1;
   }
 
 
-  private createRowData(): HTMLElement {
+  getRowsNumber(): number {
+    return this.rowsContainer.nativeElement.children.length - 1;
+  }
+
+
+  private createCell(): HTMLElement {
     const rowData = this.renderer2.createElement('td');
     const text = this.renderer2.createText('...');
 
@@ -37,35 +39,44 @@ export class TableComponent implements AfterViewInit {
   }
 
 
-  addNewColumn() {
+  addNewColumn(): void {
+    if (this.isEmpty) {
+      this.isEmpty = false;
+      return;
+    }
+
+    const columnsNumber = this.getColumnsNumber();
     const columnsContainer = this.columnsContainer.nativeElement;
     const rowsContainer = this.rowsContainer.nativeElement;
     const newColumn = this.renderer2.createElement('th');
-    const text = this.renderer2.createText('Header' + this.columnsNumber.toString());
+    const text = this.renderer2.createText('Header' + columnsNumber.toString());
 
     this.renderer2.appendChild(newColumn, text);
 
-    for (let i = 1; i < this.rowsNumber - 1; ++i) {
-      this.renderer2.appendChild(rowsContainer.children[i], this.createRowData());
+    for (let i = 0; i < this.getRowsNumber(); ++i) {
+      this.renderer2.appendChild(rowsContainer.children[i], this.createCell());
     }
 
-    this.renderer2.insertBefore(columnsContainer, newColumn, columnsContainer.children[this.columnsNumber - 1])
-
-    ++this.columnsNumber;
+    this.renderer2.insertBefore(columnsContainer, newColumn, columnsContainer.children[columnsNumber])
   }
 
 
-  addNewRow() {
+  addNewRow(): void {
+    if (this.isEmpty) {
+      this.isEmpty = false;
+      return;
+    }
+
+    const columnsNumber = this.getColumnsNumber();
+    const rowsNumber = this.getRowsNumber();
     const rowsContainer = this.rowsContainer.nativeElement;
     const newRow = this.renderer2.createElement('tr');
 
-    for (let i = 0; i < this.columnsNumber - 2; ++i) {
-      this.renderer2.appendChild(newRow, this.createRowData());
+    for (let i = 0; i < columnsNumber - 1; ++i) {
+      this.renderer2.appendChild(newRow, this.createCell());
     }
 
-    this.renderer2.appendChild(newRow, this.createRowData());
-    this.renderer2.insertBefore(rowsContainer, newRow, rowsContainer.children[this.rowsNumber - 1])
-
-    ++this.rowsNumber;
+    this.renderer2.appendChild(newRow, this.createCell());
+    this.renderer2.insertBefore(rowsContainer, newRow, rowsContainer.children[rowsNumber])
   }
 }
