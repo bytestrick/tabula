5379a653dc56ff9f72a9ecd0a0dd4068267a44e8
Giorgio Carlino <carlinogiorgiogithub@gmail.com>
2025-03-03 07:52:39 +0100

    Aggiunti i componenti home e table-card

---
+++ frontend/src/app/home/table-card/table-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-card.component.html',
+  styleUrl: './table-card.component.css'
+})
+export class TableCardComponent {
+
+}
