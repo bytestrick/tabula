21d8dafaa014a46855803e6a44a39c88c1e0d667
Giorgio Carlino <carlinogiorgiogithub@gmail.com>
2025-03-04 22:50:28 +0100

    Aggiunta la creazione e la modifica di una table-card

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, ComponentRef, ElementRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {TableCardComponent} from './table-card/table-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  protected title: String = '';
+  protected description: String = '';
 
+  @ViewChild('tableCardContainer', { read: ViewContainerRef }) private tableCardContainerRef!: ViewContainerRef;
+  @ViewChild('createTableCardForm') private form!: ElementRef<HTMLFormElement>;
+
+  protected createTableCard(): void {
+    if (!this.form) {
+      return;
+    }
+
+    if (this.form.nativeElement.checkValidity()) {
+      if (!this.tableCardContainerRef) {
+        console.error("tableCardContainer è undefined, impossibile creare il componente.");
+        return;
+      }
+      let newTableCard: ComponentRef<TableCardComponent> = this.tableCardContainerRef.createComponent(TableCardComponent);
+      newTableCard.instance.setTitle(this.title);
+      newTableCard.instance.setDescription(this.description);
+
+      this.title = '';
+      this.description = '';
+      this.form.nativeElement.classList.remove('was-validated');
+    }
+    else {
+      this.form.nativeElement.classList.add('was-validated');
+    }
+  }
+
+  protected cancelCreateTableCard(): void {
+    this.title = '';
+    this.description = '';
+    this.form.nativeElement.classList.remove('was-validated');
+  }
 }
--- frontend/src/app/home/table-card/table-card.component.ts
+++ frontend/src/app/home/table-card/table-card.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-table-card',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf,
+  ],
   templateUrl: './table-card.component.html',
   styleUrl: './table-card.component.css'
 })
 export class TableCardComponent {
+  protected title: String = '';
+  protected description: String = '';
+  protected formTitle: String = '';
+  protected formDescription: String = '';
+  protected inEditMode: boolean = false;
 
+  @ViewChild('editTableCardForm') private form!: ElementRef<HTMLFormElement>;
+
+  public edit(): void {
+    this.formTitle = this.title;
+    this.formDescription = this.description;
+    this.inEditMode = true;
+  }
+
+  public cancelEdit(): void {
+    this.inEditMode = false;
+    this.form.nativeElement.classList.remove('was-validated');
+  }
+
+  public confirmEdit(): void {
+    if (!this.form) {
+      return;
+    }
+
+    if (this.form.nativeElement.checkValidity()) {
+      this.title = this.formTitle;
+      this.description = this.formDescription;
+      this.inEditMode = false;
+      this.form.nativeElement.classList.remove('was-validated');
+    }
+    else {
+      this.form.nativeElement.classList.add('was-validated');
+    }
+  }
+
+  public setTitle(title: String): void {
+    this.title = title;
+  }
+
+  public setDescription(description: String): void {
+    this.description = description;
+  }
 }
