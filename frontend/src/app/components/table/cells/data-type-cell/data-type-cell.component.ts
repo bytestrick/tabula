d452a7922ccb782a33213861da2d31fc39fa243c
icsp <alfredocarlinogithub@gmail.com>
2025-03-05 15:22:07 +0100

    - Aggiunto il componente DataTypeCellComponent per visualizzare l'icona e il tipo di dato   nelle celle di intestazione. - Modificato TextualDataType: il metodo getDataTypeIcon() ora restituisce il componente   DataTypeCellComponent anziché un HTMLElement. - Aggiornata l'interfaccia IDataType per uniformare il tipo restituito da getDataTypeIcon()   a Type<BaseCellComponent>. - Refactoring di TableComponent per integrare il nuovo componente e gestire correttamente   la selezione delle celle e l'input popup.

---
+++ frontend/src/app/components/table/cells/data-type-cell/data-type-cell.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {BaseCellComponent} from '../base-cell-component';
+
+@Component({
+  selector: 'app-data-type-cell',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-type-cell.component.html',
+  styleUrl: './data-type-cell.component.css'
+})
+export class DataTypeCellComponent extends BaseCellComponent {
+  private readonly defaultValue: string = 'New Column';
+
+  override value: any = this.defaultValue;
+
+
+  override setValue(value: any): void {
+    if (value === null || value === '')
+      this.value = this.defaultValue;
+    else
+      this.value = value;
+  }
+}
--- frontend/src/app/model/data-type.interface.ts
+++ frontend/src/app/model/data-type.interface.ts
@@ -5,6 +5,6 @@ import {BaseCellComponent} from '../components/table/cells/base-cell-component';
 export interface IDataType {
   getInputComponent(): Type<BaseInputComponent>;
   getNewDataType(): IDataType; // Da togliere se non serve tenere traccia di tutte le celle. Vedi table.ts
-  getDataTypeIcon(): HTMLElement; // Ritorna la parte che viene mostrata sulla tabella per far capire che tipo di dato c'è lungo la colonna.
+  getDataTypeIcon(): Type<BaseCellComponent>; // Ritorna la parte che viene mostrata sulla tabella per far capire che tipo di dato c'è lungo la colonna.
   getCellComponent(): Type<BaseCellComponent>; // Ritorna l'elemento che andrà messo come cella nella tabella.
 }
