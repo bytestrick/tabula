28264d20c3faf4e926ebf33bff782f2b18be01ca
Mario D'Andrea <goffredo2004@gmail.com>
2025-03-23 16:49:41 +0100

    Add page not found fallback component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { AppComponent } from './app.component';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {AppComponent} from './app.component';
 import {authGuard} from './auth/auth.guard';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent, canActivate: [authGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  {path: '', component: AppComponent, canActivate: [authGuard]},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
