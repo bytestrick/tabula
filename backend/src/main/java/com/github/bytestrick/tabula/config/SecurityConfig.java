9a579b63eecd7a764b96d61a9c596659a94ba0ec
Mario D'Andrea <goffredo2004@gmail.com>
2025-04-10 16:08:15 +0200

    use PATCH instead of POST to reset the password

--- backend/src/main/java/com/github/bytestrick/tabula/config/SecurityConfig.java
+++ backend/src/main/java/com/github/bytestrick/tabula/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Angular URl
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
