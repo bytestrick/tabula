22934923c7c3e956c4ad6a46c07a9b1147e2923b
Mario D'Andrea <goffredo2004@gmail.com>
2025-04-11 10:37:16 +0200

    store secrets in a .env file instead of application.properties
    old committed secrets have been replaced

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.postgresql:postgresql")
     implementation("com.nimbusds:nimbus-jose-jwt:10.2")
+    implementation("me.paulschwarz:spring-dotenv:4.0.0")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     compileOnly("org.projectlombok:lombok")
