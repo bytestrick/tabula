48562288b99e573d76079ee73d46800b0c136e40
Giorgio Carlino <carlinogiorgiogithub@gmail.com>
2025-04-18 20:50:40 +0200

    Modificato lo schema del DB e riscritto i campi in snake case

--- doc/schema.sql
+++ doc/schema.sql
@@ -20,3 +20,110 @@ CREATE TABLE invalid_jwts
     token           VARCHAR(500) PRIMARY KEY,
     expiration_date TIMESTAMP NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION delete_expired_jwt()
+    RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM invalid_jwts WHERE expiration_date < NOW();
+END;
+$$ LANGUAGE plpgsql;
+
+--SELECT cron.schedule('delete-expired-jwt-daily', '0 0 * * *', 'SELECT delete_expired_jwt()');
+
+--TODO: cron job to delete all users that are not verified and have an expired OTP
+
+
+CREATE TABLE data_type 
+(
+    id              SMALLSERIAL PRIMARY KEY,
+    name            VARCHAR(500) NOT NULL,
+
+    UNIQUE (name)
+);
+
+INSERT INTO data_type (name)
+VALUES
+    ('Textual'),
+    ('Numeric'),
+    ('Monetary');
+
+CREATE TABLE my_table 
+(
+    id              UUID PRIMARY KEY
+);
+
+
+CREATE TABLE my_column
+(
+    id              UUID PRIMARY KEY,
+    my_table        UUID,
+    data_type       SMALLSERIAL,
+    column_index    INT NOT NULL,
+
+    FOREIGN KEY (my_table) REFERENCES my_table (id),
+    FOREIGN KEY (data_type) REFERENCES data_type (id),
+    UNIQUE(column_index, my_table)
+);
+
+
+CREATE TABLE my_row
+(
+    id UUID         PRIMARY KEY,
+    my_table        UUID,
+    row_index       INT NOT NULL,
+
+    FOREIGN KEY (my_table) REFERENCES my_table (id),
+    UNIQUE(row_index, my_table)
+);
+
+
+CREATE TABLE cell
+(
+    id              UUID PRIMARY KEY,
+    my_row          UUID,
+    my_column       UUID,
+    value           VARCHAR(1000),
+
+    FOREIGN KEY (my_column) REFERENCES my_column(id),
+    FOREIGN KEY (my_row) REFERENCES my_row(id)
+);
+
+
+CREATE OR REPLACE FUNCTION deleteColumnCellsOnColumnDelete()
+RETURNS trigger AS $$
+BEGIN
+    DELETE FROM cell WHERE my_column = OLD.id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER onDeleteColumn
+AFTER DELETE ON my_column
+FOR EACH ROW
+EXECUTE FUNCTION deleteColumnCellsOnColumnDelete();
+
+
+CREATE OR REPLACE FUNCTION deleteRowCellsOnRowDelete()
+RETURNS trigger AS $$
+BEGIN
+    DELETE FROM cell WHERE my_row = OLD.id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER onDeleteRow
+AFTER DELETE ON my_row
+FOR EACH ROW
+EXECUTE FUNCTION deleteRowCellsOnRowDelete();
+
+
+CREATE TABLE table_card (
+    id uuid primary key default gen_random_uuid(),
+    title varchar(50) not null default '',
+    description varchar(500) not null default '',
+    creation_date timestamp not null default now(),
+    last_edit_date timestamp default null
+);
