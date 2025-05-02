c3071aacba08615bf2419b4e7107158c622bf0ed
Mario D'Andrea <goffredo2004@gmail.com>
2025-05-02 08:22:44 +0200

    fix: zero-pad minutes on table last edit time in the home

--- frontend/src/app/home/pretty-date.pipe.ts
+++ frontend/src/app/home/pretty-date.pipe.ts
@@ -1,42 +1,40 @@
-import { Pipe, PipeTransform} from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'prettyDate',
   standalone: true,
 })
 export class PrettyDatePipe implements PipeTransform {
+  // Numero di secondi in un anno, un mese, una settimana e un giorno
+  static readonly intervals = {
+    'year': 31536000,
+    'month': 2592000,
+    'week': 604800,
+    'day': 86400
+  };
 
-    transform(value?: Date | string): string {
-    if (! value) return '';
+  transform(value?: Date | string): string {
+    if (!value) return '';
 
     // Non si cosiderano i tempi futuri al tempo in 'now' che porterebbero a un 'diff' negativo
-    const now: Date = new Date();
-    const nowS: number = Math.floor(now.getTime() / 1000);
+    const nowS: number = Math.floor(new Date().getTime() / 1000);
     const date: Date = new Date(value);
-    const dateS: number = Math.floor(date.getTime() / 1000)
+    const dateS: number = Math.floor(date.getTime() / 1000);
     const diff: number = nowS - dateS;
 
     const monthName: string = Intl.DateTimeFormat(undefined, {month: 'short'}).format(date);
 
-    // Numero di secondi in un anno, un mese, una settimana e un giorno
-    const intervals = {
-      'year': 31536000,
-      'month': 2592000,
-      'week': 604800,
-      'day': 86400
-    };
 
-    const sameDay: boolean = Math.floor(diff / intervals['day']) === 0;
-    const sameWeek: boolean = Math.floor(diff / intervals['week']) === 0;
-    const sameMonth: boolean = Math.floor(diff / intervals['month']) === 0;
-
+    const sameDay: boolean = Math.floor(diff / PrettyDatePipe.intervals['day']) === 0;
+    const sameWeek: boolean = Math.floor(diff / PrettyDatePipe.intervals['week']) === 0;
+    const sameMonth: boolean = Math.floor(diff / PrettyDatePipe.intervals['month']) === 0;
 
     if (sameDay || sameWeek) {
-      return date.getHours() + ':' + date.getMinutes();
+      return date.getHours() + ':' + date.getMinutes().toString().padStart(2, '0') ;
     }
 
     if (sameMonth) {
-        return date.getDay() + ' ' + monthName;
+      return date.getDay() + ' ' + monthName;
     }
 
     return date.getDay() + ' ' + monthName + ' ' + date.getFullYear();
