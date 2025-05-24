2c98b6f32898dfd0dce0fa1be5a589ed78d08d7b
Giorgio Carlino <carlinogiorgiogithub@gmail.com>
2025-05-24 16:05:31 +0200

    fix: in precedenza, il tema tornava a quello predefinito del browser al ricaricamento della pagina. Ora viene mantenuto correttamente in base alla scelta dell'utente.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,27 +17,11 @@ import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component'
 export class AppComponent implements OnInit {
 
   private popUpManager: PopUpManagerService = inject(PopUpManagerService);
-
   _ = inject(ThemeService);
-
   @ViewChild('popUpContainer', { read: ViewContainerRef, static: true }) popUpContainer!: ViewContainerRef;
 
 
   ngOnInit() {
-    AppComponent.updateColorScheme();
     this.popUpManager.setPopUpContainer(this.popUpContainer);
-  }
-
-
-  /**
-   * Sync the app color scheme with the OS color scheme
-   */
-  private static updateColorScheme() {
-    const darkColorSchemeQuery = matchMedia('(prefers-color-scheme: dark)');
-    const onChange = (isDark: boolean) => {
-      document.documentElement.setAttribute('data-bs-theme', isDark ? 'dark' : 'light');
-    };
-    darkColorSchemeQuery.addEventListener('change', e => onChange(e.matches));
-    onChange(darkColorSchemeQuery.matches); // at first load
   }
 }
