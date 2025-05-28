b86e4f71d9661d3004b5ba44a71529f0557b7759
Mario D'Andrea <goffredo2004@gmail.com>
2025-05-28 18:56:31 +0200

    Revert "fix: crossorigin"
    This reverts commit c4a20ac0012475427bdce73234edc252daf0b16d.

--- backend/src/main/java/com/github/bytestrick/tabula/config/SecurityConfig.java
+++ backend/src/main/java/com/github/bytestrick/tabula/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/*", configuration);
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
